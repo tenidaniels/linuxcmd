1b9436d61488eaf9f8576dcbea8216af4d395e2b
Oluwateniola DANIEL Olubowale <52575436+tenidaniels@users.noreply.github.com>
2025-02-14 18:41:14 +0100

    Update main.bash

--- main.bash
+++ main.bash
@@ -1,2 +1,13 @@
 ls /etc/nginx/sites-enabled/
 #list all sites (nano) in nginx
+
+curl -k https://182.168.4.38:8443
+#curl: A command-line tool to transfer data from or to a server.
+#-k: (insecure mode) It tells curl to ignore SSL certificate validation. Useful if the server has a self-signed or untrusted SSL certificate.
+
+
+sudo tail -f /var/log/nginx/api-nginx.frontend_upstream.log
+#tail -f: tail shows the last few lines of a file.
+#-f (follow) keeps the output live, updating as new log entries are added.
+#/var/log/nginx/api-nginx.frontend_upstream.log: The log file being monitored.
+
